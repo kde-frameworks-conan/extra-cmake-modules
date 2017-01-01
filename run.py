7ab91a744085a49e09b4a55272a584f0c301ef3f
Russell Greene <russellgreene8@gmail.com>
2017-01-01 12:20:50 -0700

    Maybe fix packaging

--- run.py
+++ run.py
@@ -7,17 +7,6 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(visual_runtimes=["MT", "MD"])
     
-    if platform.system() == "Windows":
-        builder.add_common_builds(visual_versions=[14])
-    
-    if platform.system() == "Linux":
-        for ver in ["4.8", "4.9", "5.4", "6.2"]:
-            for bt in ["Debug", "Release"]:
-                builder.add(
-                    {"arch": "x86_64", 
-                     "build_type": bt,
-                     "compiler": "gcc",
-                     "compiler.version": ver,
-                     "compiler.libcxx": "libstdc++11"})
+    builder.add_common_builds()
     
     builder.run()
